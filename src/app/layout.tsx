7e422ff2ae0cd1cb4daca8236270717d72f428f0
FutsalDex <futsaldex@gmail.com>
2025-10-24 11:19:19 +0000

    untime Error
    Hydration failed because the server rendered HTML didn't m

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es" style={{}}>
+    <html lang="es">
       <body className={cn('antialiased', fontBody.variable, fontHeadline.variable)}>
         <div className="flex min-h-screen w-full flex-col">
           <Header />
