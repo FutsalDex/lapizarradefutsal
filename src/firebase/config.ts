6fe0839075149a11fa1cc9a83f5be8351e8f231b
FutsalDex <futsaldex@gmail.com>
2025-10-24 11:41:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "lapizarra-95eqd",
+  "appId": "1:303306895935:web:463b38b92cc31842ccfe8a",
+  "apiKey": "AIzaSyA2XHO-VnkYuAwx3-cQ8xrWb3gzdzvTSow",
+  "authDomain": "lapizarra-95eqd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "303306895935"
+};
