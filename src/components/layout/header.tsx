32cf34ce43515bec6a2771f0d4101840fa6c288d
FutsalDex <futsaldex@gmail.com>
2025-10-25 17:26:19 +0000

    Sigue sin mostrarse

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -103,7 +103,7 @@ export function Header() {
                     </Link>
                   </SheetClose>
                 ))}
-                {!isUserLoading && !user && (
+                {!user && (
                   <SheetClose asChild>
                     <Link
                       href="/acceso"
