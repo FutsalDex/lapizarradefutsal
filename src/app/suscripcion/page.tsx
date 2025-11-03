32419dd0dc5ea8f704907bdfa88d45c42c9b6c59
FutsalDex <futsaldex@gmail.com>
2025-11-03 07:18:51 +0000

    Cambia el sistema de conseguir puntos. Ademas de: Gana puntos aportando

--- src/app/suscripcion/page.tsx
+++ src/app/suscripcion/page.tsx
@@ -134,7 +134,7 @@ export default function SuscripcionPage() {
                  <Card className="lg:col-span-2">
                     <CardHeader>
                         <CardTitle className="text-xl flex items-center gap-2"><Star className="text-amber-400"/>Mis Puntos</CardTitle>
-                         <CardDescription>Gana puntos aportando ejercicios a la comunidad y canjéalos por meses gratis.</CardDescription>
+                         <CardDescription>Gana puntos aportando ejercicios, invita a tus amigos (50 puntos por cada amigo que se suscriba) y canjéalos por meses gratis.</CardDescription>
                     </CardHeader>
                     <CardContent className="space-y-4">
                         <div className="text-center">
