c69ce5da0c7465e27b513110dbb8463ba3a982f5
FutsalDex <futsaldex@gmail.com>
2025-11-04 16:46:25 +0000

    problema

--- next.config.js
+++ next.config.js
@@ -1,25 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    images: {
-      remotePatterns: [
-        { protocol: 'https', hostname: 'placehold.co', pathname: '/**' },
-        { protocol: 'https', hostname: 'picsum.photos', pathname: '/**' },
-        { protocol: 'https', hostname: 'firebasestorage.googleapis.com', pathname: '/**' },
-        { protocol: 'https', hostname: 'i.ibb.co', pathname: '/**' },
-      ],
-    },
-    experimental: {
-      allowedDevelopmentOrigins: [
-        'https://*.cloudworkstations.dev',
-        'https://*.firebaseapp.com',
-        'http://localhost:9100',
-        'https://localhost:9100',
-      ],
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'placehold.co', pathname: '/**' },
+      { protocol: 'https', hostname: 'picsum.photos', pathname: '/**' },
+      { protocol: 'https', hostname: 'firebasestorage.googleapis.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'i.ibb.co', pathname: '/**' },
+    ],
+  },
+};
+
+export default nextConfig;
